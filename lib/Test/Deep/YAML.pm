be401ab97badb75b03d985831f4e4c0a513343db
Karen Etheridge <ether@cpan.org>
2013-05-27 13:22:12 -0700

    initial commit, via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.006, github profile.

---
+++ lib/Test/Deep/YAML.pm
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+package Test::Deep::YAML;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+
+=head1 FUNCTIONS/METHODS
+
+=begin :list
+
+* C<foo>
+
+=end :list
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Test-Deep-YAML>
+(or L<bug-Test-Deep-YAML@rt.cpan.org|mailto:bug-Test-Deep-YAML@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+...
+
+=cut
